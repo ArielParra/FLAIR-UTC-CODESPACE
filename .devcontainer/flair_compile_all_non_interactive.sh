963e311c11802620528db46645b34c85e077b8d1
Ariel Parra <66755357+ArielParra@users.noreply.github.com>
2025-11-06 22:41:04 +0100

    fixed sed and parallel flair_compile_all

--- .devcontainer/flair_compile_all_non_interactive.sh
+++ .devcontainer/flair_compile_all_non_interactive.sh
@@ -5,21 +5,32 @@ NB_THREADS=$(nproc)
 FLAIR_BUILD_DIR="$FLAIR_ROOT/flair-build"
 IDE_SCRIPT="cmake_codelite_outofsource.sh"
 
+# Clean build directory
 rm -rf "$FLAIR_BUILD_DIR/*"
 mkdir -p "$FLAIR_BUILD_DIR"
 cd "$FLAIR_BUILD_DIR"
 
+# Run CMake for all toolchains
 "$FLAIR_ROOT/flair-src/scripts/$IDE_SCRIPT" "$FLAIR_ROOT/flair-src/"
 
 toolchains=($OECORE_CMAKE_TOOLCHAINS)
+
+# Compile all architectures in parallel
 for arch in "${toolchains[@]}"; do
-    cd "$FLAIR_BUILD_DIR/build_$arch"
-    make -j"$NB_THREADS"
-    make install
+    (
+        cd "$FLAIR_BUILD_DIR/build_$arch"
+        make -j"$NB_THREADS"
+        make install
+    ) &
 done
 
+# Wait for all parallel builds to finish
+wait
+
+# Ensure doc directory exists
 mkdir -p "$FLAIR_ROOT/flair-install/doc"
 
+# Generate documentation using Doxygen from the last arch
 DOXYGEN=$(eval "echo \"\$OECORE_${arch^^}_NATIVE_SYSROOT\"")/usr/bin/doxygen
 if [ -x "$DOXYGEN" ]; then
     "$DOXYGEN" "$FLAIR_ROOT/flair-src/lib/Doxyfile.in"
